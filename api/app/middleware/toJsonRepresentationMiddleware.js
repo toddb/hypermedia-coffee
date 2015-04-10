65db248f1a4f1eda888769ee9f99f537beca5bc0
toddb <todd@goneopen.com>
2015-04-10 16:12:35 +1200

    Add documentation

--- api/app/middleware/toJsonRepresentationMiddleware.js
+++ api/app/middleware/toJsonRepresentationMiddleware.js
@@ -9,6 +9,16 @@ module.exports = function toJsonRepresentation(app) {
       if (err) return res.status(501).send(err);
     }
 
+    /**
+     * @callback Predicate
+     * @param {(Feed|FeedItem)} representation
+     */
+
+    /**
+     * Sends resource across the wire as a json representation
+     * @param representation
+     * @param {Predicate} fn predicate on a representation
+     */
     var toJsonRepresentation = function (representation, fn) {
       if (fn != undefined) {
         fn(representation)
@@ -18,19 +28,33 @@ module.exports = function toJsonRepresentation(app) {
     };
 
     /**
-     *
+     * Sends resource across the wire as a FeedItem json representation
      * @param url
-     * @param fn
+     * @param {Predicate} fn predicate on a representation
      */
     res.toRepresentation = function (url, fn) {
       toJsonRepresentation(new FeedItem(url, {}), fn);
     };
 
+    /**
+     * Sends Feed resource across the wire as a FeedItem json representation
+     * @param {Error} err
+     * @param {object} doc resource to be returned
+     * @param {string} url to identify the representation self link relation
+     * @param {Predicate} fn predicate on a representation
+     */
     res.toFeedRepresentation = function (err, doc, url, fn) {
       checkError(err);
       toJsonRepresentation(new Feed(url, doc), fn);
     };
 
+    /**
+     * Sends FeedItem resource across the wire as a FeedItem json representation
+     * @param {Error} err
+     * @param {object} doc resource to be returned
+     * @param {string} url to identify the representation self link relation
+     * @param {Predicate} fn predicate on a representation
+     */
     res.toFeedItemRepresentation = function (err, doc, url, fn) {
       checkError(err);
       res.etag(doc.id, doc.modified);
@@ -38,6 +62,11 @@ module.exports = function toJsonRepresentation(app) {
       toJsonRepresentation(new FeedItem(url, doc), fn);
     };
 
+    /**
+     * Sends resource across the wire as a FeedItem json representation with a 201 status and Location header
+     * @param {Error} err
+     * @param {string} id
+     */
     res.toCreatedRepresentation = function (err, id) {
       checkError(err);
       var url = res.locals.request_url + id;
@@ -48,6 +77,10 @@ module.exports = function toJsonRepresentation(app) {
       });
     };
 
+    /**
+     * Sends resource across the wire as a FeedItem json representation with a 204
+     * @param {Error} err
+     */
     res.toNoResponseRepresentation = function (err) {
       checkError(err);
       res.status(204);
--- api/app/representation/feed.js
+++ api/app/representation/feed.js
@@ -6,6 +6,23 @@ var _ = require('underscore')
       }
     };
 
+/**
+ * @example
+ * var representation = new Representation('http://example.com/api', {});
+ *
+ * Output:
+ *
+ * {
+ *  links: [
+ *    { rel: 'self', href: 'http://example.com/api', type:'application/json'}
+ * ]}
+ *
+ *
+ *
+ * @param {string} url The uri of the relationship
+ * @param {object} doc The resource document
+ * @constructor
+ */
 function Representation(url, doc) {
 
   this.links = [];
@@ -24,46 +41,145 @@ function Representation(url, doc) {
   }
 }
 
-Representation.prototype.addLink = fluent(function (rel, type, url) {
-  if (_.isUndefined(type))
+/**
+ * Adds a link relation to `links`
+ *
+ * @example
+ *
+ *  representation.addLink('up', 'http://example.com/order/');
+ *
+ * Output:
+ *
+ * links: [
+ *  { rel: 'self' ... would already exist },
+ *  { rel: 'up', href: 'http://example.com/order/', type: 'application/json' }
+ * ]
+ *
+ * @param {String} relationshipType
+ * @param {?String} [mediaType=application/json]
+ * @return {String} url The uri of the relationship
+ */
+Representation.prototype.addLink = fluent(function (relationshipType, mediaType, url) {
+  if (_.isUndefined(mediaType))
     return
 
   if (_.isUndefined(url)) {
-    url = type;
-    type = 'application/json';
+    url = mediaType;
+    mediaType = 'application/json';
   }
 
-  this.links.push({rel: rel, type: type, href: url});
+  this.links.push({rel: relationshipType, type: mediaType, href: url});
 });
 
+/**
+ * @callback RelationshipTypeCallback
+ * @param {string} relationshipType
+ */
+/**
+ * Adds multiple link relations to `links`
+ *
+ * @example
+ *
+ *   // url is already http://example.com/order/123/
+ *   rels = ['pay']
+ *   representation.addLinks(rels, function (rel) {
+ *     return url + rel;
+ *   })
+ *
+ * Output:
+ *
+ * links: [
+ *  { rel: 'self' ... would already exist },
+ *  { rel: 'pay', href: 'http://example.com/order/123/pay', type: 'application/json' }
+ * ]
+ *
+ *
+ * @param {Array.<string>} transition
+ * @param {RelationshipTypeCallback} fn
+ */
 Representation.prototype.addLinks = fluent(function (transition, fn) {
   var _this = this;
-  _.each(transition, function (rel) {
-    _this.addLink(rel, fn(rel));
+  _.each(transition, function (relationshipType) {
+    _this.addLink(relationshipType, fn(relationshipType));
   });
 });
 
+/**
+ * Adds a self link
+ *
+ * @example
+ *
+ * representation.selfLink(url);
+ *
+ * Output:
+ *
+ * links: [
+ *  { rel: 'self', href: 'http://example.com/api/', type: 'application/json' },
+ * ]
+ *
+ * @param {string} url The uri of the relationship
+ * @private
+ */
 Representation.prototype.selfLink = fluent(function (url) {
   this.addLink('self', url);
 });
 
-Representation.prototype.addCollection = fluent(function (url, docs) {
+/**
+ *  @example
+ *
+ * function(doc){
+ *    return {
+ *      id: url + doc._id,
+        title: doc.type
+ *    };
+ * }
+ *
+ * @callback Predicate
+ * @param {object} doc
+ */
+
+/**
+ * Add a collection to the `items`
+ *
+ * @example
+ *
+ * representation.addCollection(url, []);
+ *
+ * Output:
+ *
+ * links: [
+ *  { rel: 'self', href: 'http://example.com/api/', type: 'application/json' },
+ * ]
+ *
+ * @param {string} url The uri of the relationship
+ * @param {Array.<Object>} docs The list of resources
+ * @param {?Predicate} fn Callback that must return an object with {id:string, title:string}
+ */
+Representation.prototype.addCollection = fluent(function (url, docs, fn) {
   if (_.isUndefined(url))
     return;
 
   if (_.isEmpty(docs))
     return;
 
+  if (_.isUndefined(fn)) {
+    fn = function (doc) {
+      return {
+        id: url + doc._id,
+        title: doc.type
+      };
+    }
+  }
   this.items = [];
   _.each(docs, function (doc) {
-    this.items.push({
-      id: url + doc._id,
-      title: doc.type
-    });
+    this.items.push(fn(doc));
   }, this);
 
 });
 
+/**
+ * Returns the representation
+ */
 Representation.prototype.toJSON = fluent(function () {
 });
 
